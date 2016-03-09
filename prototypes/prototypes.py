344d557a87a27bc5871a13e673b91339b5cf9193
Eric Kureck <kureck@gmail.com>
2016-03-09 00:46:06 -0300

    creates prototypes

---
+++ prototypes/prototypes.py
@@ -0,0 +1,33 @@
+import sys
+import os
+
+from django.conf import settings
+
+BASE_DIR = os.path.dirname(__file__)
+
+DEBUG = os.environ.get('DEBUG', 'on') == 'on'
+
+SECRET_KEY = os.environ.get('SECRET_KEY', '52hwdx2_2pnsksw-en0s!o$+5@xy&p5_6s29=(k1k@c&bjn&6b')
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+
+settings.configure(
+    DEBUG=DEBUG,
+    SECRET_KEY=SECRET_KEY,
+    ALLOWED_HOSTS=ALLOWED_HOSTS,
+    ROOT_URLCONF='sitebuilder.urls',
+    MIDDLEWARE_CLASSES=(),
+    INSTALLED_APPS=(
+        'django.contrib.staticfiles',
+        'django.contrib.webdesign',
+        'sitebuilder',
+    ),
+    STATIC_URL='/static/',
+    SITE_PAGES_DIRECTORY=os.path.join(BASE_DIR, 'pages'),
+)
+
+
+if __name__ == '__main__':
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
